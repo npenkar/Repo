687227e724693f8a7630c0a7ec01ed029c700cba
Lenovo <Lenovo@DESKTOP-UQFTAD3>
2021-04-21 17:11:36 +0530

    accordion 1 open fixed

--- Test/src/Oprations.java
+++ Test/src/Oprations.java
@@ -1,50 +1,58 @@
-import java.util.Random;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Oprations {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		Runtime.getRuntime().exec("taskkill /F /IM ChromeDriver.exe");
+//		Runtime.getRuntime().exec("taskkill /F /IM Chrome.exe");
 		
-		Random random = new Random();
-		int x = random.nextInt(999);  
-		String str = "CANON 418 CYAN 8278";
-		String output1;
+		System.setProperty("webdriver.chrome.driver", "C:\\Np\\Dev\\Eclipse\\Repo\\Test\\jars\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
 		
-//		String output = (str.length()>10) ? str.substring (0,10).concat(""):str;
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		String login = "https://sso.gem.gov.in/ARXSSO/oauth/doLogin";
+		String addnewproduct = "https://admin-mkp.gem.gov.in/admin/cat/catalog/angular_catalog/#!/catalog/new?bnid=home_offi_offi_prin_comp";
+
+		String uname = Files.readAllLines(Paths.get("C:\\Np\\Dev\\Eclipse\\Repo\\Test\\resource\\creden.txt")).get(0);
+		String pwd = Files.readAllLines(Paths.get("C:\\Np\\Dev\\Eclipse\\Repo\\Test\\resource\\creden.txt")).get(1);
+		System.out.println("	Reading Username and Password from file... ");
 		
-		if ((str.length()>15))
+		driver.manage().window().maximize();
+		driver.get(login);
+		driver.findElement(By.id("loginid")).sendKeys(uname);
+		Thread.sleep(100);
+		driver.findElement(By.xpath("/html[1]/body[1]/section[5]/div[1]/div[1]/div[2]/div[1]/div[1]/form[1]/div[4]/div[2]/button[1]")).click();
+		Thread.sleep(100);
+		driver.findElement(By.id("captcha_math")).click();
+		Thread.sleep(600);
+		driver.findElement(By.xpath("//input[@id='password']")).click();
+		driver.findElement(By.xpath("//input[@id='password']")).clear();
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys(pwd);
+		Thread.sleep(600);
+		Thread.sleep(600);
+		driver.findElement(By.cssSelector("#loginFrm>div.row>div:nth-child(1)>button")).click();
+		Thread.sleep(600);
+		
+		driver.get(addnewproduct);
+		Thread.sleep(6000);
+		WebElement accordion = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div/new-component/div/form/uib-accordion/div/ng-form[1]/div/div[1]/h4/a/span/div/div[2]"));
+		String getAttribute = accordion.getAttribute("class");
+		System.out.println("	getAttribute: " + getAttribute);
+		
+		if(getAttribute.contains("input-group-item fa fa-2 circle-right fa-chevron-circle-right"))
 		{
-			 output1 = str.substring (0,14) +" "+ x;
-//			System.out.println(output1+" "+ x);
+			accordion.click();
 		}
-		else 
-		{
-			 output1 = str +" "+ x;
-//			System.out.println(output1+" "+ x);
-		}
-		
-//		str = str.substring(Math.min(str.length(), 5));
-		
-//		int strln = str.length();
-//		 String result1 = truncate(str, 2);
-	        System.out.println(output1);
-//		if (strln > 14)
-//		{
-//			
-//		}
-//		
-		
-		
-		
-		
-//		else
-//		{
-//			str = str.substring(0, Math.min(str.length(), 14)) + x;
-//			
-//		}
-//		System.out.println(output);
-		
-//		System.out.println(x);
-		
+	
+	
 	}
 
 	
