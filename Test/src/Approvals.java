cbcaa758bd20642f7033f40344e1e4c5600f693d
admin <admin@admin-PC>
2021-11-19 09:57:59 +0530

    chromedriver96 added approval

--- Test/src/Approvals.java
+++ Test/src/Approvals.java
@@ -15,10 +15,10 @@ public class Approvals
 {
 	public static void main(String[] args) throws InterruptedException, AWTException, Exception 
 	{
-		Runtime.getRuntime().exec("taskkill /F /IM ChromeDriver.exe");
+		Runtime.getRuntime().exec("taskkill /F /IM ChromeDriver96.exe");
 		Runtime.getRuntime().exec("taskkill /F /IM Chrome.exe");
 		
-		System.setProperty("webdriver.chrome.driver", "C:\\Np\\Dev\\Eclipse\\Repo\\Test\\jars\\chromedriver94.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Np\\Dev\\Eclipse\\Repo\\Test\\jars\\chromedriver96.exe");
 		WebDriver driver = new ChromeDriver();
 		
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
