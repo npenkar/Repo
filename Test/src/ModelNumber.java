255d60dc653f3e49cfec3c0822c7de1bb1649222
admin <admin@admin-PC>
2022-02-25 22:24:25 +0530

    Pooja code updated

--- Test/src/ModelNumber.java
+++ Test/src/ModelNumber.java
@@ -314,9 +314,9 @@ public class ModelNumber
 							PartNew = Part + " " + y;
 						}
 						String TitleNew;
-						if ((Title.length() > 19)) 
+						if ((Title.length() > 95)) 
 						{
-							TitleNew = Title.substring(0, 19); //+ " " + z;
+							TitleNew = Title.substring(0, 95); //+ " " + z;
 						} 
 						else 
 						{
@@ -328,42 +328,49 @@ public class ModelNumber
 						System.out.println("Trimmed Title he ye : "+TitleNew+ '\n');
 						System.out.println("NewModel ye hai : "+NewModel);
 						System.out.println("===========================================" + '\n');
-						System.out.println(Make);
-						System.out.println(Model);
-						System.out.println(ModelNew);
-						System.out.println(Part);
-						System.out.println(PartNew);
-						System.out.println(OEMBrand);
-						System.out.println(OEMModel);
-						System.out.println(PrinterModel);
-						System.out.println(Yield);
-						System.out.println(img1);
+						System.out.println("Make: "+Make);
+						System.out.println("Model: "+Model);
+						System.out.println("ModelNew :"+ModelNew);
+						System.out.println("Part :"+Part);
+						System.out.println("PartNew :"+PartNew);
+						System.out.println("OEMBrand :"+OEMBrand);
+						System.out.println("OEMModel :"+OEMModel);
+						System.out.println("PrinterModel :"+PrinterModel);
+						System.out.println("Yield :"+Yield);
+						System.out.println("img1 :"+img1);
 //						System.out.println(img2);
 //						System.out.println(img3);
-						System.out.println(MRPIntNew);
-						System.out.println(SellPriceIntNew);
-						System.out.println(SellPriceLowRate);
+						System.out.println("MRPIntNew: "+MRPIntNew);
+						System.out.println("SellPriceIntNew: "+SellPriceIntNew);
+						System.out.println("SellPriceLowRate: "+SellPriceLowRate);
 						System.out.println("String Price: " + FinalSellPrice);
 						
 						driver.get(addnewproduct);
 						Thread.sleep(2000);		
 						
 //						driver.findElement(By.xpath("//img[@id='floxChatCloseImage']")).click();
-						if (str.contentEquals("KT") || (str.contentEquals("kt")))
+						if ((str.contentEquals("KT")) || (str.contentEquals("kt")))
 						{
 						driver.findElement(By.xpath("//body/div[@id='page']/div[@id='bd']/div[@id='content-slot']/div[1]/div[1]/new-component[1]/div[1]/form[1]/uib-accordion[1]/div[1]/ng-form[1]/div[1]/div[2]/div[1]/fieldset[1]/div[3]/div[2]/select[1]")).click();	
 						Thread.sleep(600);
 						driver.findElement(By.xpath("//body/div[@id='page']/div[@id='bd']/div[@id='content-slot']/div[1]/div[1]/new-component[1]/div[1]/form[1]/uib-accordion[1]/div[1]/ng-form[1]/div[1]/div[2]/div[1]/fieldset[1]/div[3]/div[2]/select[1]")).sendKeys("KANT");
 						Thread.sleep(600);
 						}
-						else if  ((str.contentEquals("PJ") || (str.contentEquals("pj"))) ||  (str.contentEquals("YH") || (str.contentEquals("yh"))))
+						else if   ((str.contentEquals("PJ")) || (str.contentEquals("pj")))
+					    {
+						driver.findElement(By.xpath("//body/div[@id='page']/div[@id='bd']/div[@id='content-slot']/div[1]/div[1]/new-component[1]/div[1]/form[1]/uib-accordion[1]/div[1]/ng-form[1]/div[1]/div[2]/div[1]/fieldset[1]/div[3]/div[2]/select[1]")).click();	
+						Thread.sleep(600);
+						driver.findElement(By.xpath("//body/div[@id='page']/div[@id='bd']/div[@id='content-slot']/div[1]/div[1]/new-component[1]/div[1]/form[1]/uib-accordion[1]/div[1]/ng-form[1]/div[1]/div[2]/div[1]/fieldset[1]/div[3]/div[2]/select[1]")).sendKeys("Jeny");	
+						Thread.sleep(600);
+					    }
+						else if  ((str.contentEquals("YH")) || (str.contentEquals("yh")))
 					    {
 						driver.findElement(By.xpath("//body/div[@id='page']/div[@id='bd']/div[@id='content-slot']/div[1]/div[1]/new-component[1]/div[1]/form[1]/uib-accordion[1]/div[1]/ng-form[1]/div[1]/div[2]/div[1]/fieldset[1]/div[3]/div[2]/select[1]")).click();	
 						Thread.sleep(600);
 						driver.findElement(By.xpath("//body/div[@id='page']/div[@id='bd']/div[@id='content-slot']/div[1]/div[1]/new-component[1]/div[1]/form[1]/uib-accordion[1]/div[1]/ng-form[1]/div[1]/div[2]/div[1]/fieldset[1]/div[3]/div[2]/select[1]")).sendKeys("Tinu");	
 						Thread.sleep(600);
 					    }
-						else if  (str.contentEquals("ES") || (str.contentEquals("es")))
+						else if  ((str.contentEquals("ES")) || (str.contentEquals("es")))
 						{
 						driver.findElement(By.xpath("//body/div[@id='page']/div[@id='bd']/div[@id='content-slot']/div[1]/div[1]/new-component[1]/div[1]/form[1]/uib-accordion[1]/div[1]/ng-form[1]/div[1]/div[2]/div[1]/fieldset[1]/div[3]/div[2]/select[1]")).click();	
 						Thread.sleep(600);
@@ -411,13 +418,12 @@ public class ModelNumber
 						robot.keyRelease(KeyEvent.VK_ENTER);
 						Thread.sleep(5000);
 		
-					    if(str.contentEquals("KT") || str.contains("kt") || str.contains("kantado") || str.contains("k") || str.contentEquals("E") || str.contains("e") || str.contains("eshani") || str.contains("ESHANI"))
+					    if(str.contentEquals("KT") || str.contains("kt") || str.contentEquals("ES") || str.contains("es"))
 					    {
-					    driver.findElement(By.xpath("//body/div[@id='page']/div[@id='bd']/div[@id='content-slot']/div[1]/div[1]/new-component[1]/div[1]/form[1]/uib-accordion[1]/div[1]/ng-form[3]/div[1]/div[2]/div[1]/fieldset[1]/selling-as[1]/div[1]/div[1]/div[2]/select[1]")).sendKeys("e"
-					    		+ "");
+					    driver.findElement(By.xpath("//body/div[@id='page']/div[@id='bd']/div[@id='content-slot']/div[1]/div[1]/new-component[1]/div[1]/form[1]/uib-accordion[1]/div[1]/ng-form[3]/div[1]/div[2]/div[1]/fieldset[1]/selling-as[1]/div[1]/div[1]/div[2]/select[1]")).sendKeys("e");
 						Thread.sleep(100);
 					    }
-					    else if (str.contentEquals("p") || str.contains("P") || str.contains("pooja") || str.contentEquals("Y") || str.contains("y") || str.contains("yash"))
+					    else if (str.contentEquals("pj") || str.contains("PJ") || str.contentEquals("YH") || str.contains("yh"))
 					    {
 //Resellers Details
 						driver.findElement(By.xpath("//body/div[@id='page']/div[@id='bd']/div[@id='content-slot']/div[1]/div[1]/new-component[1]/div[1]/form[1]/uib-accordion[1]/div[1]/ng-form[3]/div[1]/div[2]/div[1]/fieldset[1]/selling-as[1]/div[1]/div[1]/div[2]/select[1]")).sendKeys("Resellers");
