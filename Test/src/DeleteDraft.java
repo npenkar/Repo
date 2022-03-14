a66713ce9d0e7dc1f50725bd756252702601fcd7
admin <admin@admin-PC>
2022-03-14 22:06:07 +0530

    DELETE DARAFT FIXES ADDED

--- Test/src/DeleteDraft.java
+++ Test/src/DeleteDraft.java
@@ -20,8 +20,8 @@ public class DeleteDraft {
 
 	public static void main(String[] args) throws Exception{
 		long start = System.currentTimeMillis(); 
-		Runtime.getRuntime().exec("taskkill /F /IM ChromeDriver.exe");
-		Runtime.getRuntime().exec("taskkill /F /IM Chrome.exe");
+		Runtime.getRuntime().exec("taskkill /F /IM ChromeDriver99.exe");
+		Runtime.getRuntime().exec("taskkill /F /IM chrome.exe");
 		System.setProperty("webdriver.chrome.driver", "C:\\Np\\Dev\\Eclipse\\Repo\\Test\\jars\\chromedriver99.exe");
 
 		WebDriver driver = new ChromeDriver();
@@ -49,26 +49,48 @@ public class DeleteDraft {
 		Thread.sleep(500); 
 
 		driver.get(draft);
-		Thread.sleep(3000); 
-		driver.findElement(By.xpath("//*[@id=\"catalog-button\"]")).click();
+		Thread.sleep(4000); 
+		driver.findElement(By.xpath(".//span[contains(text(),'More')]")).click();
 		
 		Thread.sleep(500);
 		driver.findElement(By.xpath("//a[contains(text(),'Draft products')]")).click();
-		Thread.sleep(3000);
+		Thread.sleep(5000);
 	     
 		int count = 0;
 		for(int i=0; i<10000; i++)
 		{	
-		driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div/index-component/div[2]/div/div[4]/table/tbody/tr[1]/td[12]/span[3]/a")).click();
-		Thread.sleep(500);
-		driver.findElement(By.xpath("//button[contains(text(),'Yes')]")).click();
-		Thread.sleep(4000);
-
-		count++;
-		long ExecutionTime = (System.currentTimeMillis() - start);
-		long ETS = ExecutionTime/1000 ;
-		System.out.println(" ====>  "+count+" : Stock DELETED in " + ETS +" seconds");
-		continue;
+//			driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div/index-component/div[2]/div/ul/li[3]/a")).click();
+//			Thread.sleep(2000);
+//			
+			for(int j=1; j<=10; j++)
+			{
+				
+//                WebElement element = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div/index-component/div[2]/div/div[4]/table/tbody/tr[8]/td[12]/span[3]/a"));                        
+//                ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", element);
+////                driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div/index-component/div[2]/div/ul/li[3]/a")).click(); // PAGE5
+//                driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+////                Thread.sleep(3000);
+//                driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div/index-component/div[2]/div/div[4]/table/tbody/tr[10]/td[12]/span[3]/a")).click();
+//                Thread.sleep(500);
+//                driver.findElement(By.xpath("//button[contains(text(),'Yes')]")).click();
+//                Thread.sleep(5000);
+                
+				driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+				WebElement element = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div/index-component/div[2]/div/div[4]/table/tbody/tr["+j+"]/td[12]/span[3]"));
+//				Thread.sleep(500);
+				JavascriptExecutor executor = (JavascriptExecutor) driver;
+				executor.executeScript("arguments[0].click();", element);
+//				((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", element);
+				Thread.sleep(500);
+				driver.findElement(By.xpath("//button[contains(text(),'Yes')]")).click();
+				Thread.sleep(4000);
+				
+				count++;
+				long ExecutionTime = (System.currentTimeMillis() - start);
+				long ETS = ExecutionTime/1000 ;
+				System.out.println(" ====>  "+count+" : Stock DELETED in " + ETS +" seconds");
+			}
+		
 		}
 	}
 }	
